a74513e52dd479ff6d9c36349ed83e84ea632939
Gaël Cadoret <g.cadoret@24sevres.com>
2022-09-02 10:29:07 +0200

    clean code

--- src/components/accounts/controllers/getTransactionsByAccNumber.js
+++ src/components/accounts/controllers/getTransactionsByAccNumber.js
@@ -1,22 +1,22 @@
 const { getTransactionsByAccNumber } = require("../services");
 const { successResponse, errorResponse } = require("../../../../modules/helpers");
-const { validateAccountsBody } = require("../validators");
 
 module.exports = async (req, res) => {
     try {
-      // const hasValidationError = validateAccountsBody(req.body);
-
-      // if (hasValidationError) {
-      //   return res.status(400).json(errorResponse("Something get wrong with the request"));
-      // }
-
+      // TODO : add validation and security check
       const { acc_number } = req.params;
 
-      return res.status(200).json(successResponse({ data: getTransactionsByAccNumber(acc_number) }));
+      return res.status(200).json(
+          successResponse({
+              data: getTransactionsByAccNumber(acc_number)
+          })
+      );
 
     } catch (e) {
       console.error(e.message);
 
-      return res.status(500).json(errorResponse("Something get wrong with the request"));
+      return res.status(500).json(
+          errorResponse("Something get wrong with the request")
+      );
     }
 };
--- src/components/accounts/services/getTransactionsByAccNumber.js
+++ src/components/accounts/services/getTransactionsByAccNumber.js
@@ -4,5 +4,9 @@ const { transactions } = require("../../../../__mocks__/accounts.json");
 const findAccNumber = (accNumber) => (transaction) =>
     transaction.acc_number === accNumber;
 
+/**
+ * @param {string} accNumber
+ * @return {*} all transaction for specific account number
+ */
 module.exports = (accNumber) =>
     transformer(transactions).find(findAccNumber(accNumber));
